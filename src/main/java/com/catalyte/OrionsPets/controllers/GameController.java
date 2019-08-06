c56b694b9848c0b3b37481be6f61a2f9311f52a6
Orion Wolf-Hubbard <orion@Orions-MacBook-Pro.local>
2019-08-06 10:41:17 -0400

    added score check

--- src/main/java/com/catalyte/OrionsPets/controllers/GameController.java
+++ src/main/java/com/catalyte/OrionsPets/controllers/GameController.java
@@ -1,7 +1,6 @@
 package com.catalyte.OrionsPets.controllers;
 
 import com.catalyte.OrionsPets.models.Game;
-import com.catalyte.OrionsPets.models.Player;
 import com.catalyte.OrionsPets.repositories.GameRepository;
 import com.catalyte.OrionsPets.repositories.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +32,8 @@ public class GameController {
             playerOne.equals(playerTwo)
         ) {
             return "Please enter valid players";
+        } else if (scoreOne < 0 || scoreTwo < 0 || scoreOne == scoreTwo) {
+            return "Please enter valid scores";
         } else {
             int count = (int) gameRepository.count() + 1;
             Game game = new Game(playerOne, playerTwo, scoreOne, scoreTwo, count);
