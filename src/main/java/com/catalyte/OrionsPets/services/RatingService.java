4e3ce7dbdc304f529cf9f4055c6d4500ca50cbf5
Orion Wolf-Hubbard <orion@Orions-MBP.fios-router.home>
2019-08-11 08:36:36 -0400

    zachRating

--- src/main/java/com/catalyte/OrionsPets/services/RatingService.java
+++ src/main/java/com/catalyte/OrionsPets/services/RatingService.java
@@ -4,6 +4,7 @@ import com.catalyte.OrionsPets.models.Player;
 
 public class RatingService {
     private static int K = 50;
+    private static int zachK = 20;
 
     public static void rate(Player player1, Player player2, int score1, int score2) {
         double transform1 = Math.pow(10, player1.getRating() / 400) / (Math.pow(10, player1.getRating() / 400) + Math.pow(10, player2.getRating() / 400));
@@ -21,8 +22,8 @@ public class RatingService {
         double newRating1 = player1.getRating() + K * (fraction1 - transform1);
         double newRating2 = player2.getRating() + K * (fraction2 - transform2);
 
-        double newZachting1 = player1.getZachRating() + K * ((score1 > score2 ? 1 : 0) - zachform1);
-        double newZachting2 = player2.getZachRating() + K * ((score2 > score1 ? 1 : 0) - zachform2);
+        double newZachting1 = player1.getZachRating() + zachK * ((score1 > score2 ? 1 : 0) - zachform1);
+        double newZachting2 = player2.getZachRating() + zachK * ((score2 > score1 ? 1 : 0) - zachform2);
 
         player1.setRating(newRating1);
         player2.setRating(newRating2);
